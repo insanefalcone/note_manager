9ba8dbb978db6c9020672f38c1a5f5d5facfa14a
insanefalcone <yarikyakovickiy@gmail.com>
2024-12-23 22:16:48 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username="Кирилл"
+print("имя пользователя", username)
+title="Заметка №1"
+print("заголовок заметки", title)
+content="Заметка для дз"
+print("описание заметки", content)
+status="Выполняется"
+print("статус заметки", status)
+created_date="16-12-2024"
+print("дата создания заметки", created_date)
+issue_date="20-12-2024"
+print("дата истечения заметки", issue_date)
