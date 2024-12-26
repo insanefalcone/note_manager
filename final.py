b4a080474aa24782d03d2f78ad52e561312591f0
insanefalcone <yarikyakovickiy@gmail.com>
2024-12-26 21:45:50 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -1,13 +1,20 @@
-username=input("введите имя пользователя ")
-title1=input("введите заголовок заметки №1 ")
-title2=input("введите заголовок заметки №2 ")
-title3=input("введите заголовок заметки №3 ")
-a = [title1, title2, title3]
-content = input("введите описание заметки ")
-status = input("введите статус выполнения заметки ")
-created_date = input("введите дату начала в формате день-месяц-год ")
-temp_create_date = created_date[0:5]
-issue_date = input("введите дату планируемого окончания в формате день-месяц-год")
-temp_issue_date = issue_date[0:5]
-b = [username,[a], content, status, temp_create_date, temp_issue_date]
-print(b)
+note = {
+     "username": input("введите имя пользователя "),
+     "content": input("введите описание заметки "),
+     "titles": [input("Введите текст заголовка 1 "), input("Введите текст заголовка 2 "), input("Введите текст заголовка 3 ")],
+     "status": input("введите статус выполнения заметки "),
+     "created_date": input("введите дату начала в формате день-месяц-год "),
+     "issue_date": input("введите дату планируемого окончания в формате день-месяц-год ")}
+username = note["username"]
+content = note["content"]
+titles = note["titles"]
+status = note["status"]
+created_date = note["created_date"]
+issue_date = note["issue_date"]
+print("имя пользователя", username)
+print("описание заметки", content)
+print("текст заголовка", titles)
+print("статус", status)
+print("дата начала", created_date[0:5])
+print("дата планируемого окончания", issue_date[0:5])
+
